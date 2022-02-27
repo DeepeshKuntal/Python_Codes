ed70b45ae9ea42725d832866ecbef808f329c4ed
RedNinjaX <100291374+DeepeshKuntal@users.noreply.github.com>
2022-02-28 00:24:56 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,22 @@
+#Inputs
+a= int(input("Enter Your First Number: "))
+a= int(a)
+b= int(input("Ener Your Second Number: "))
+b= int(b)
+c= (input("Enter The Operation: "))
+#If command to make it work
+if c == "+":
+    print("Addition Of The Numbers Is ", a+b)
+
+if c == "-":
+    print("Diffrence Of The Numbers Is ", a-b)
+
+if c == "x":
+    print("The Multiplication Of The Numbers Is ", a*b)
+
+if c == "/":
+    print("The Divison Of The Numbers Is", a/b)
+
+#Statement to make multiplication work,
+if c == "X":
+    print("Dont Use CapsLock Pls Try x")
